0ea041fa62b0172bd02229451eb15d058956883c
Albert Strzyżewski <albert.strzyzewski@sobre.pl>
2023-11-15 19:57:48 +0100

    Konfiguracja datocms-a

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,24 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require("dotenv").config();
+
 module.exports = {
   siteMetadata: {
     title: `your-best-party-page`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    "gatsby-plugin-styled-components", 
+    "gatsby-plugin-image",
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        // You can find your read-only API token under the Settings > API tokens
+        // section of your administrative area. Make sure to grant both CDA and CMA permissions.
+        apiToken: process.env.GATSBY_DATO_CMS_KEY,
+      },
+    },
+  ]
 };
