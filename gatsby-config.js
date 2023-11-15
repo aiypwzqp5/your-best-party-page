657ea7dca5b168b874d83d63022868c8d3a16227
Albert Strzyżewski <albert.strzyzewski@sobre.pl>
2023-11-15 20:34:06 +0100

    KONFIGURACJA PLUGINÓW DO SEO STRONY

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 module.exports = {
   siteMetadata: {
     title: `your-best-party-page`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://yourbestpartypage.pl`
   },
   plugins: [
     "gatsby-plugin-styled-components", 
@@ -18,6 +18,68 @@ module.exports = {
         // You can find your read-only API token under the Settings > API tokens
         // section of your administrative area. Make sure to grant both CDA and CMA permissions.
         apiToken: process.env.GATSBY_DATO_CMS_KEY,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `your-best-party-page`,
+        short_name: `YourBestParty`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: 'src/images/icon.png'
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://yourbestpartypage.pl',
+        sitemap: 'https://yourbestpartypage.pl/sitemap-index.xml',
+        policy: [process.env.NODE_ENV === 'production' 
+          ? {userAgent: '*', allow: '/'} 
+          : {userAgent: '*', disallow: '/'}
+        ]
+      }
+    },
+    {
+      resolve: "gatsby-plugin-sitemap",
+      options: {
+        query: `
+          {
+            allSitePage {
+              nodes {
+                path
+              }
+            }
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+          }
+        `,
+        resolveSiteUrl: ({
+          site: {
+            siteMetadata: { siteUrl },
+          },
+        }) => siteUrl,
+        resolvePages: ({
+          allSitePage: { nodes },
+          site: {
+            siteMetadata: { siteUrl },
+          },
+        }) =>
+          nodes.map(({ path }) => ({
+            url: `${siteUrl}${path}`,
+            path,
+            changefreq: "daily",
+            priority: 0.7,
+          })),
+        serialize: ({ path }) => ({
+          url: path,
+        }),
       },
     },
   ]
