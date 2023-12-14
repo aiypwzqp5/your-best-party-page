7d0c9f3bf515fed7119d3808c28714cf717ab908
Albert Strzyżewski <albert.strzyzewski@sobre.pl>
2023-12-14 19:18:16 +0100

    ACTIVE CLASS NAME

--- src/components/atoms/Link/Link.js
+++ src/components/atoms/Link/Link.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import { StyledLink } from './Link.styles'
+import React from "react";
+import { StyledLink } from "./Link.styles";
 
-const Link = ({ url, ariaLabel, className, children }) => 
-    <StyledLink to={url} aria-label={ariaLabel} className={className}>
-        {children}
-    </StyledLink>
+const Link = ({ url, ariaLabel, className, children }) => (
+  <StyledLink to={url} aria-label={ariaLabel} className={className} activeClassName="active">
+    {children}
+  </StyledLink>
+);
 
-export default Link
+export default Link;
--- src/components/atoms/Link/Link.styles.js
+++ src/components/atoms/Link/Link.styles.js
@@ -1,29 +1,30 @@
-import styled from "styled-components"
-import { Link } from "gatsby"
-import { focusVisibleStyles } from "../../../styles/sharedStyles"
+import styled from "styled-components";
+import { Link } from "gatsby";
+import { focusVisibleStyles } from "../../../styles/sharedStyles";
 
 export const StyledLink = styled(Link)`
-    text-decoration: none;
-    color: var(--white);
-    font-size: 20px;
-    transition: color 250ms linear;
-    cursor: pointer;
+  text-decoration: none;
+  color: var(--white);
+  font-size: 20px;
+  transition: color 250ms linear;
+  cursor: pointer;
 
-    &.color--black {
-        color: var(--black);
+  &.color--black {
+    color: var(--black);
 
-        @media only screen and (min-width: 768px){
-            color: var(--white);
-        }
+    @media only screen and (min-width: 768px) {
+      color: var(--white);
     }
+  }
 
-    &.color--always-black {
-        color: var(--black);
-    }
+  &.color--always-black {
+    color: var(--black);
+  }
 
-    &:hover {
-        color: var(--red);
-    }
+  &:hover,
+  &.active {
+    color: var(--red);
+  }
 
-    ${focusVisibleStyles};
-`
+  ${focusVisibleStyles};
+`;
